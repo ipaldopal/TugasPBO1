79f8742b17a0fe48275bef827269015f968e30c0
ipaldopal <71812489+ipaldopal@users.noreply.github.com>
2021-03-16 21:13:55 +0700

    Update and rename README.md to MenghitungGallon.java

---
+++ MenghitungGallon.java
@@ -0,0 +1,21 @@
+package TugasPBOGallon;
+
+import java.util.Scanner;
+
+public class Gallontgs1 {
+
+    public static void main(String[] args) {
+        Scanner ambil = new Scanner(System.in);
+
+        double Gallons;
+        double count;
+
+        System.out.println("Konversi Satuan Liter Galon");
+        System.out.println("\nMasukkan Jumlah Galon : ");
+
+        Gallons=ambil.nextDouble();
+        count=Gallons*3.785f;
+
+        System.out.printf("%.0f Galon = %.3f Liter \n\n",Gallons,count);
+    }
+}
